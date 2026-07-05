c07979419f0fccc0ff8599c69323e436e4fabbac
matheodrd <matheodurand14@gmail.com>
2026-07-05 14:27:02 +0200

    feat: update log-output-server to read from the writer's file output

--- apps/log-output/server/app/main.py
+++ apps/log-output/server/app/main.py
@@ -1,43 +1,22 @@
-import asyncio
-import logging
-from contextlib import asynccontextmanager
-from datetime import UTC, datetime
-from typing import TYPE_CHECKING, Annotated
-from uuid import uuid4
+from pathlib import Path
+from typing import Annotated
 
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI
+from fastapi.responses import PlainTextResponse
 
-if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+OUTPUT_DIR = Path("./files/")
+OUTPUT_FILE = Path(OUTPUT_DIR / "output")
 
-
-async def log_loop(app: FastAPI) -> None:
-    while True:
-        logger.info(f"{datetime.now(UTC)}: {app.state.random_string}")
-        await asyncio.sleep(5)
+app = FastAPI(title="Log output")
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
-    app.state.random_string = str(uuid4())
-    asyncio.create_task(log_loop(app))
-    yield
+def read_random_string() -> str:
+    with OUTPUT_FILE.open("r") as f:
+        return f.read()
 
 
-app = FastAPI(title="Log output", lifespan=lifespan)
-
-logger = logging.getLogger("uvicorn.error")
-
-
-def get_random_string(request: Request) -> str:
-    return request.app.state.random_string
-
-
-@app.get("/status")
+@app.get("/", response_class=PlainTextResponse)
 async def get_status(
-    random_string: Annotated[str, Depends(get_random_string)],
-) -> dict[str, str]:
-    return {
-        "timestamp": str(datetime.now(UTC)),
-        "random_string": random_string,
-    }
+    random_string: Annotated[str, Depends(read_random_string)],
+) -> str:
+    return random_string
