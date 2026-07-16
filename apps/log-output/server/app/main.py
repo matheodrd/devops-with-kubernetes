f9d6dcd0a2aeedacb06256a122dfeb9473bc69b4
matheodrd <matheodurand14@gmail.com>
2026-07-16 10:42:44 +0200

    feat: get pings count from ping-pong /pings endpoint instead of a file

--- apps/log-output/server/app/main.py
+++ apps/log-output/server/app/main.py
@@ -1,11 +1,14 @@
+import os
 from pathlib import Path
 from typing import Annotated
 
+import niquests
 from fastapi import Depends, FastAPI
 from fastapi.responses import PlainTextResponse
 
 LOG_OUTPUT_FILE = Path("./files/output")
-PING_PONG_COUNTER_FILE = Path("/usr/share/data/ping-pong/counter")
+PING_PONG_API_HOST = os.getenv("PING_PONG_API_HOST", "ping-pong")
+PING_PONG_API_PORT = os.getenv("PING_PONG_API_PORT", "1236")
 
 app = FastAPI(title="Log output")
 
@@ -18,19 +21,20 @@ def read_random_string() -> str:
         return "No string generated yet"
 
 
-def read_ping_pong_count() -> str:
-    if not PING_PONG_COUNTER_FILE.exists():
-        return "0"
-    try:
-        with PING_PONG_COUNTER_FILE.open("r") as f:
-            return f.read()
-    except Exception:
-        return "0"
+async def get_pings() -> int:
+    response = await niquests.aget(
+        f"http://{PING_PONG_API_HOST}:{PING_PONG_API_PORT}/pings"
+    )
+    response.raise_for_status()
+
+    if not response.content:
+        return 0
+    return response.json()["data"]
 
 
 @app.get("/", response_class=PlainTextResponse)
 async def get_status(
     random_string: Annotated[str, Depends(read_random_string)],
-    ping_pong_count: Annotated[str, Depends(read_ping_pong_count)],
+    pings: Annotated[int, Depends(get_pings)],
 ) -> str:
-    return f"{random_string}.\nPing / Pongs: {ping_pong_count}"
+    return f"{random_string}.\nPing / Pongs: {pings}"
